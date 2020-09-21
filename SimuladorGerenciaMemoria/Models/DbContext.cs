8b27147bbef39257b531c4ac8688c4da0b62d282
LUCAS FERREIRA DE ALMEIDA <lucas.ferreira.13@hotmail.com>
2020-09-21 12:40:31 -0300

    first

---
+++ SimuladorGerenciaMemoria/Models/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using SimuladorGerenciaMemoria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimuladorGerenciaMemoria.Data
+{
+    public class SimuladorContext : DbContext
+    {
+        public SimuladorContext(DbContextOptions<SimuladorContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
