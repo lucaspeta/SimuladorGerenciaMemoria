cc3aa6c0c7325441269112192f4b7ee47afe69e1
LUCAS FERREIRA DE ALMEIDA <lucas.ferreira.13@hotmail.com>
2020-10-07 17:11:49 -0300

    simulation_and_memorie_crud

---
+++ SimuladorGerenciaMemoria/Controllers/MemoriesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SimuladorGerenciaMemoria;
+using SimuladorGerenciaMemoria.Models;
+
+namespace SimuladorGerenciaMemoria.Controllers
+{
+    public class MemoriesController : Controller
+    {
+        private readonly SimuladorContext _context;
+
+        public MemoriesController(SimuladorContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Memories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Memories.ToListAsync());
+        }
+
+        // GET: Memories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var memory = await _context.Memories
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (memory == null)
+            {
+                return NotFound();
+            }
+
+            return View(memory);
+        }
+
+        // GET: Memories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Memories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,CreateDate")] Memory memory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(memory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(memory);
+        }
+
+        // GET: Memories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var memory = await _context.Memories.FindAsync(id);
+            if (memory == null)
+            {
+                return NotFound();
+            }
+            return View(memory);
+        }
+
+        // POST: Memories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,CreateDate")] Memory memory)
+        {
+            if (id != memory.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(memory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemoryExists(memory.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(memory);
+        }
+
+        // GET: Memories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var memory = await _context.Memories
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (memory == null)
+            {
+                return NotFound();
+            }
+
+            return View(memory);
+        }
+
+        // POST: Memories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var memory = await _context.Memories.FindAsync(id);
+            _context.Memories.Remove(memory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MemoryExists(int id)
+        {
+            return _context.Memories.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ SimuladorGerenciaMemoria/Controllers/SimulationsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SimuladorGerenciaMemoria;
+using SimuladorGerenciaMemoria.Models;
+
+namespace SimuladorGerenciaMemoria.Controllers
+{
+    public class SimulationsController : Controller
+    {
+        private readonly SimuladorContext _context;
+
+        public SimulationsController(SimuladorContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Simulations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Simulations.ToListAsync());
+        }
+
+        // GET: Simulations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var simulation = await _context.Simulations
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (simulation == null)
+            {
+                return NotFound();
+            }
+
+            return View(simulation);
+        }
+
+        // GET: Simulations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Simulations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,CreateDate")] Simulation simulation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(simulation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(simulation);
+        }
+
+        // GET: Simulations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var simulation = await _context.Simulations.FindAsync(id);
+            if (simulation == null)
+            {
+                return NotFound();
+            }
+            return View(simulation);
+        }
+
+        // POST: Simulations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,CreateDate")] Simulation simulation)
+        {
+            if (id != simulation.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(simulation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SimulationExists(simulation.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(simulation);
+        }
+
+        // GET: Simulations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var simulation = await _context.Simulations
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (simulation == null)
+            {
+                return NotFound();
+            }
+
+            return View(simulation);
+        }
+
+        // POST: Simulations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var simulation = await _context.Simulations.FindAsync(id);
+            _context.Simulations.Remove(simulation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SimulationExists(int id)
+        {
+            return _context.Simulations.Any(e => e.ID == id);
+        }
+    }
+}
