5a6310c3856ec9bf8aa29d69d8f8fb5505371be1
LUCAS FERREIRA DE ALMEIDA <lucas.ferreira.13@hotmail.com>
2020-10-22 16:23:00 -0300

    list and creat layout update

--- SimuladorGerenciaMemoria/Models/Memory.cs
+++ SimuladorGerenciaMemoria/Models/Memory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using SimuladorGerenciaMemoria.Classes;
@@ -14,11 +15,16 @@ namespace SimuladorGerenciaMemoria.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+        [DisplayName("Nome")]
         public string Name { get; set; }
+        [DisplayName("Tamanho")]
         public long Size { get; set; }
+        [DisplayName("Tamanho dos frames")]
         public long FramesSize { get; set; }
+        [DisplayName("Quantidade de frames")]
         public long FramesQTD { get; set; }
         public List<Frame> Frames { get; set; }
+        [DisplayName("Data de criação")]
         public DateTime CreateDate { get; set; }
     }
 
--- SimuladorGerenciaMemoria/Models/Simulation.cs
+++ SimuladorGerenciaMemoria/Models/Simulation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,7 +11,9 @@ namespace SimuladorGerenciaMemoria.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+        [DisplayName("Nome")]
         public string Name { get; set; }
+        [DisplayName("Data de criação")]
         public DateTime CreateDate { get; set; }
     }
 }
